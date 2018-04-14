296aec5d51dc71c0904477352e2da0d10f08a6b3
Futa Hirakoba <korosuke613613@gmail.com>
2018-04-14 21:54:39 +0900

    fix warning

--- src/main/java/bwdm/informationStore/IfElseExprSyntaxTree.java
+++ src/main/java/bwdm/informationStore/IfElseExprSyntaxTree.java
@@ -1,9 +1,5 @@
 package bwdm.informationStore;
 
-import com.fujitsu.vdmj.lex.LexException;
-import com.fujitsu.vdmj.syntax.ParserException;
-
-import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,23 +8,22 @@ public class IfElseExprSyntaxTree {
 
 	IfNode root;
 	List<String> ifElses;
-	int count = 0;
+	private int count = 0;
 
 
-	public IfElseExprSyntaxTree(String _ifExpressionBoby) throws ParserException, LexException, IOException{
+	public IfElseExprSyntaxTree(String _ifExpressionBoby){
 		shapeIfElseBody(_ifExpressionBoby);
 		generateIfElseSyntaxTree();
 		//recursiveReturnNodeFind(root);
 	} //end constructor
 
 	public IfNode getRoot() { return root; }
-	public void printAllNodes() { printNodesRecursively(root); }
 
 	/*
      * shaping of passed ifElseBody
      */
-	private void shapeIfElseBody(String _ifElseBody) throws LexException, ParserException, IOException {
-		ifElses = new ArrayList<String>();
+	private void shapeIfElseBody(String _ifElseBody) {
+		ifElses = new ArrayList<>();
 		String ifElseBody = _ifElseBody;
 		ifElseBody = ifElseBody.replace("(", "").
 				                replace(")", "").
@@ -42,11 +37,9 @@ public class IfElseExprSyntaxTree {
 
 
 	//if式構文木を作る
-	private void generateIfElseSyntaxTree()
-			throws IOException {
-
+	private void generateIfElseSyntaxTree() {
 		//rootの準備
-		ifElses.get(count++); //最初はifなので無視
+		count++; //最初はifなので無視
 		String currentLine = ifElses.get(count++); //これは最初のifの条件式
 		//多分
 		root = generateIfNode(currentLine, null, 0);
@@ -55,8 +48,7 @@ public class IfElseExprSyntaxTree {
 		//これだけでおｋ
 	}
 
-	private IfNode generateIfNode(String _condition, IfNode _parentNode, int _nodeLevel)
-			throws IOException {
+	private IfNode generateIfNode(String _condition, IfNode _parentNode, int _nodeLevel) {
 		IfNode ifNode = new IfNode(_condition, _nodeLevel);
 		ifNode.parentNode = _parentNode;
 		String nextToken;
@@ -73,7 +65,7 @@ public class IfElseExprSyntaxTree {
 
 		//else 特にすることは無いので無視
 		//ということはif_elseファイルからelseを消しても問題無し？
-		nextToken = ifElses.get(count++);
+		//nextToken = ifElses.get(count++);
 
 		//elseの次、falseNode
 		//falseNode
@@ -96,61 +88,4 @@ public class IfElseExprSyntaxTree {
 		returnNode.parentNode = parentNode;
 		return returnNode;
 	}
-
-
-	//ReturnNodeの発見とそこに至る為に必要な条件式とその真偽値
-	private void recursiveReturnNodeFind(Node node) {
-		if(node.isIfNode) {
-			IfNode ifNode = (IfNode)node;
-			recursiveReturnNodeFind(ifNode.conditionTrueNode);
-			recursiveReturnNodeFind(ifNode.conditionFalseNode);
-		} else { //ReturnNodeならば
-			//returnValues.add(node.getConditionOrReturnStr());
-			//ArrayList<ConditionAndBoolean> tmp = new ArrayList<ConditionAndBoolean>();
-			Node tmpNode = node;
-			while(tmpNode != null){ //下のbreak文が
-				//tmp.add(new ConditionAndBoolean(tmpNode.parentNode.getConditionOrReturnStr(), tmpNode.isTrueNode));
-				tmpNode = tmpNode.parentNode;
-				if(tmpNode.parentNode == null) break;
-			}
-		}
-	}
-
-	//nodeを末端まで再帰的に情報を表示していく
-	private void printNodesRecursively(Node node) {
-		//親がいなければroot
-		if(node.parentNode == null){
-			System.out.println(
-					"root,      nodeID:" + node.ID +
-					" nodeLevel:" + node.nodeLevel +
-					" condition:"+node.getConditionOrReturnStr());
-			IfNode ifnode = (IfNode)node;
-			printNodesRecursively(ifnode.conditionTrueNode);
-			printNodesRecursively(ifnode.conditionFalseNode);
-			return ;
-		}
-
-		if(node.isIfNode) {
-			System.out.println(
-					"IfNode,    nodeID:" + node.ID +
-					" parentNodeID:" + node.parentNode.ID +
-					" nodeLevel:" + node.nodeLevel +
-					" boolean:" + node.isTrueNode +
-					" condition:"+node.getConditionOrReturnStr());
-			IfNode ifnode = (IfNode)node;
-
-			printNodesRecursively(ifnode.conditionTrueNode);
-			printNodesRecursively(ifnode.conditionFalseNode);
-
-		} else {
-			System.out.println(
-					"ReturnNode, nodeID:" + node.ID +
-					" parentNodeID:" + node.parentNode.ID +
-					" nodeLevel:" + node.nodeLevel +
-					" boolean:" + node.isTrueNode +
-					" Return:"+node.getConditionOrReturnStr());
-		}
-	}
-
-
 }
