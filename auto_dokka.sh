9eed91d590417ba7a0531293a1d4b503171b42a6
Futa Hirakoba <korosuke613613@gmail.com>
2018-04-20 02:23:37 +0900

    fix

--- auto_dokka.sh
+++ auto_dokka.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-if [$TRAVIS_PULL_REQUEST = "true"]; then
+if [ $TRAVIS_PULL_REQUEST = "true" ]; then
   return
 fi
 
 if [ $TRAVIS_BRANCH = "master" ] || [ $TRAVIS_BRANCH = "develop" ]; then
-  ./gradlew dokka_$current_branch
+  cd docs
+  git checkout -b gh-pages
+  git pull --depth=1 origin gh-pages
+  cd ..
+  ./gradlew dokka_$TRAVIS_BRANCH
   cd docs
   git add -A
   git commit -m "update"
