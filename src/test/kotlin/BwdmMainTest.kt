69820d5463e0575c8b43f17e98dbd0df74b6a3c9
Futa Hirakoba <korosuke613613@gmail.com>
2018-07-21 20:00:43 +0900

    fix testcase

--- src/test/kotlin/BwdmMainTest.kt
+++ src/test/kotlin/BwdmMainTest.kt
@@ -5,12 +5,4 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class BwdmMainTest {
-
-    @Test
-    fun コマンドライン引数で適当な文字を入れて例外が出るかチェック() {
-        //Assertions.assertThrows(IllegalArgumentException::class.java) {
-        //    BwdmMain.hasOnlyOptionChar("9999999")
-        //}
-        Assertions.assertEquals(BwdmMain.hasOnlyOptionChar("9999999"), false)
-    }
 }
