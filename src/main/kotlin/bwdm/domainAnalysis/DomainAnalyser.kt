f266f8d880363c24afe019d25b14298bbab411a3
Futa <korosuke613613@gmail.com>
2019-05-13 19:39:10 +0900

    fix OutPoint

--- src/main/kotlin/bwdm/domainAnalysis/DomainAnalyser.kt
+++ src/main/kotlin/bwdm/domainAnalysis/DomainAnalyser.kt
@@ -52,30 +52,16 @@ class DomainAnalyser(private val ie: InformationExtractor){
             val bool = bools[i]
             val operator = parsedCondition["operator"]
             val alith = when(bool){
-                true -> -2
-                false -> +2
+                true -> 2
+                false -> -2
             }
             expr = if (Util.getOperator(parsedCondition["left"]!!) == "+") {
-                val str = parsedCondition["left"]!!.split("+")
-                val left = ctx.mkIntConst(str[0])
-                val right = ctx.mkIntConst(str[1])
-                val arithes: ArrayList<ArithExpr> = ArrayList()
-                arithes.add(left)
-                arithes.add(right)
-                val plus = ctx.mkAdd(left, right)
-                expr = when (operator) {
-                    "<" -> ctx.mkLt(plus, ctx.mkInt(parsedCondition["right"]!!.toInt() + alith))
-                    "<=" -> ctx.mkLe(plus, ctx.mkInt(parsedCondition["right"]!!.toInt() + alith))
-                    ">" -> ctx.mkGt(plus, ctx.mkInt(parsedCondition["right"]!!.toInt() + alith))
-                    ">=" -> ctx.mkGe(plus, ctx.mkInt(parsedCondition["right"]!!.toInt() + alith))
-                    else -> null
-                }
-                if (bool)
-                    expr //満たすべき真偽(_bool)次第で、notをつける
-                else {
-                    assert(expr != null)
-                    ctx.mkNot(expr!!)
-                }
+                makePlusExpr(
+                        parsedCondition["right"]!!,
+                        operator!!,
+                        parsedCondition["left"]!!,
+                        bool,
+                        alith)
             } else {
                 makeInequalityExpr(
                         parsedCondition["right"]!!,
@@ -117,9 +103,31 @@ class DomainAnalyser(private val ie: InformationExtractor){
         return point
     }
 
+    private fun makePlusExpr(_right: String, operator: String, _left: String, bool: Boolean, alith: Int=0): BoolExpr? {
+        val str = _left.split("+")
+        val left = ctx.mkIntConst(str[0])
+        val right = ctx.mkIntConst(str[1])
+        val arithes: ArrayList<ArithExpr> = ArrayList()
+        arithes.add(left)
+        arithes.add(right)
+        val plus = ctx.mkAdd(left, right)
+        var expr: BoolExpr? = when (operator) {
+            "<" -> ctx.mkLt(plus, ctx.mkInt(_right.toInt() + alith))
+            "<=" -> ctx.mkLe(plus, ctx.mkInt(_right.toInt() + alith))
+            ">" -> ctx.mkGt(plus, ctx.mkInt(_right.toInt() + alith))
+            ">=" -> ctx.mkGe(plus, ctx.mkInt(_right.toInt() + alith))
+            else -> null
+        }
+        if (!bool){
+            assert(expr != null)
+            expr = ctx.mkNot(expr!!)
+        }
+        return expr
+    }
+
     private fun makeInequalityExpr(_right: String, operator: String, _left: String, bool: Boolean, alith: Int=0): BoolExpr? {
         val right = java.lang.Long.valueOf(_right)
-        val expr: BoolExpr? = when (operator) {
+        var expr: BoolExpr? = when (operator) {
             "<" -> ctx.mkLt(ctx.mkIntConst(_left), ctx.mkInt(right + alith))
             "<=" -> ctx.mkLe(ctx.mkIntConst(_left), ctx.mkInt(right + alith))
             ">" -> ctx.mkGt(ctx.mkIntConst(_left), ctx.mkInt(right + alith))
@@ -127,7 +135,8 @@ class DomainAnalyser(private val ie: InformationExtractor){
             else -> null
         }
         if (!bool){
-            ctx.mkNot(expr)
+            assert(expr != null)
+            expr = ctx.mkNot(expr)
         }
         return expr
     }
