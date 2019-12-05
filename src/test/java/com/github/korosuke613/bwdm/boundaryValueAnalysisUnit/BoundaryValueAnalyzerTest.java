d62fee96848935e9449f2daff0c930b3e03cfbec
Restyled.io <commits@restyled.io>
2019-12-05 17:33:47 +0000

    Restyled by clang-format

--- src/test/java/com/github/korosuke613/bwdm/boundaryValueAnalysisUnit/BoundaryValueAnalyzerTest.java
+++ src/test/java/com/github/korosuke613/bwdm/boundaryValueAnalysisUnit/BoundaryValueAnalyzerTest.java
@@ -1,25 +1,19 @@
 package com.github.korosuke613.bwdm.boundaryValueAnalysisUnit;
 
-import com.github.korosuke613.bwdm.informationStore.InformationExtractor;
 import com.fujitsu.vdmj.lex.LexException;
 import com.fujitsu.vdmj.syntax.ParserException;
 import com.github.korosuke613.bwdm.informationStore.InformationExtractor;
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
+import org.junit.jupiter.api.Test;
 
 class BoundaryValueAnalyzerTest {
 
+  @Test
+  void test() throws LexException, ParserException, IOException {
 
-	@Test
-	void test() throws LexException, ParserException, IOException {
-
-		InformationExtractor information = new InformationExtractor("./vdm_files/Arg2_Japanese.vdmpp");
-		BoundaryValueAnalyzer bvAnalyzer = new BoundaryValueAnalyzer(information, false);
-
-
-
-
-	}
-
+    InformationExtractor information =
+        new InformationExtractor("./vdm_files/Arg2_Japanese.vdmpp");
+    BoundaryValueAnalyzer bvAnalyzer =
+        new BoundaryValueAnalyzer(information, false);
+  }
 }
