c98bdf8c3a434e5274d59c8d161f654efe5b65c4
Futa Hirakoba <korosuke613613@gmail.com>
2019-12-09 20:41:03 +0900

    テストを一緒くたに

--- src/test/kotlin/com/github/korosuke613/bwdm/informationStore/InformationExtractorFunctionTest.kt
+++ src/test/kotlin/com/github/korosuke613/bwdm/informationStore/InformationExtractorFunctionTest.kt
@@ -10,20 +10,55 @@ internal class InformationExtractorFunctionTest {
     private lateinit var informationExtractor: InformationExtractor
     private lateinit var fileName: String
 
-    @BeforeEach
-    fun setup() {
-        val resource = InformationExtractor::class.java.classLoader.getResource("function.vdmpp")
-        fileName = if (resource?.path == null) "" else resource.path
-        informationExtractor = InformationExtractor(fileName)
+    @Nested
+    inner class Function {
+        @BeforeEach
+        fun setup() {
+            val resource = InformationExtractor::class.java.classLoader.getResource("function.vdmpp")
+            fileName = if (resource?.path == null) "" else resource.path
+            informationExtractor = InformationExtractor(fileName)
+        }
+
+        @Test
+        fun getFunctionName() {
+            assertEquals("うるう年判定仕様", informationExtractor.functionName)
+        }
+
+        @Test
+        fun getVdmFilePath() {
+            assertEquals(fileName, informationExtractor.vdmFilePath)
+        }
     }
 
-    @Test
-    fun getFunctionName() {
-        assertEquals("うるう年判定仕様", informationExtractor.functionName)
-    }
+    @Nested
+    inner class Operation {
+        @BeforeEach
+        fun setup(){
+            val resource = InformationExtractor::class.java.classLoader.getResource("operation.vdmpp")
+            fileName = if (resource?.path == null) "" else resource.path
+            informationExtractor = InformationExtractor(fileName)
+        }
 
-    @Test
-    fun getVdmFilePath() {
-        assertEquals(fileName, informationExtractor.vdmFilePath)
+        @Test
+        fun getOperationName1() {
+            assertEquals("うるう年判定仕様", informationExtractor.explicitOperations["うるう年判定仕様"]?.name.toString())
+        }
+
+
+        @Test
+        fun getInstanceVariables() {
+            val expected: Array<String> = arrayOf("rule1", "rule2", "rule3")
+
+            var i = 0
+            informationExtractor.instanceVariables.values.forEach { instanceVariable: TCInstanceVariableDefinition ->
+                assertEquals(expected[i], instanceVariable.name.toString())
+                i++
+            }
+        }
+
+        @Test
+        fun getVdmFilePath() {
+            assertEquals(fileName, informationExtractor.vdmFilePath)
+        }
     }
 }
