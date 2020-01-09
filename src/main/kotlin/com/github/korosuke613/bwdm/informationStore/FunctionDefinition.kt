9901f1e1dde11fb344bcf7cac026bc4582d84751
Futa Hirakoba <korosuke613613@gmail.com>
2020-01-10 04:16:07 +0900

    関数定義で定数を使えるようにした

--- src/main/kotlin/com/github/korosuke613/bwdm/informationStore/FunctionDefinition.kt
+++ src/main/kotlin/com/github/korosuke613/bwdm/informationStore/FunctionDefinition.kt
@@ -3,10 +3,12 @@ package com.github.korosuke613.bwdm.informationStore
 import com.fujitsu.vdmj.lex.LexException
 import com.fujitsu.vdmj.syntax.ParserException
 import com.fujitsu.vdmj.tc.definitions.TCExplicitFunctionDefinition
+import com.fujitsu.vdmj.tc.definitions.TCValueDefinition
 import java.io.IOException
 
 class FunctionDefinition
-(tcDefinition: TCExplicitFunctionDefinition) : Definition(tcDefinition) {
+(tcDefinition: TCExplicitFunctionDefinition,
+ private val constantValues: LinkedHashMap<String, TCValueDefinition>) : Definition(tcDefinition) {
     override var returnValue: String = tcDefinition.type.result.toString()
 
     init {
@@ -17,6 +19,12 @@ class FunctionDefinition
         ifExpressionBody = tcDefinition.body.toString()
 
         try {
+            // 定数を実数に置き換え
+            constantValues.forEach {
+                if (ifExpressionBody.contains(it.key)) {
+                    ifExpressionBody = ifExpressionBody.replace(it.key, it.value.exp.toString())
+                }
+            }
             ifElseExprSyntaxTree = IfElseExprSyntaxTree(ifExpressionBody)
         } catch (e: ParserException) {
             e.printStackTrace()
@@ -50,6 +58,14 @@ class FunctionDefinition
             var element = ifElses[i]
             if (element == "if") {
                 element = ifElses[i + 1]
+
+                // 定数を実数に置き換え
+                constantValues.forEach {
+                    if (element.contains(it.key)) {
+                        element = element.replace(it.key, it.value.exp.toString())
+                        ifElses[i + 1] = element
+                    }
+                }
                 createCompositParameters(element)
             }
         }
